be7e2c440511303b7601c6fdefff02cbeabfb0f4
Quanghihicoder <hoangquang2508@gmail.com>
2023-08-15 10:17:28 +0700

    setup database and routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,29 @@
-const express = require('express');
+// using nodemon so that you do not need to type node index.js every time new code saved
 
+// import express - is for building the Rest apis
+import express from 'express';
+
+// import body-parser - helps to parse the request and create the req.body object
+import bodyParser from "body-parser";
+
+// import cors - provides Express middleware to enable CORS with various options, connect frontend
+import cors from "cors";
+
+// import routes
+import router from "./routes/routes.js";
+
+// init express
 const app = express();
+
+// use express json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//use cors
+app.use(cors());
+
+// use router
+app.use(router);
 
 app.get('/apis', (req, res) => {
     res.set('Content-Type', 'text/html');
---
+++ backend/routes/routes.js
@@ -0,0 +1,8 @@
+// import express 
+import express from "express";
+
+// init express router
+const router = express.Router();
+
+// export default router
+export default router;
