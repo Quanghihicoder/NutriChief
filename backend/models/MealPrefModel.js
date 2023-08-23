5764cebc998925fc2d57c46eb5dcae43ed3aaf02
Quanghihicoder <hoangquang2508@gmail.com>
2023-08-23 15:08:30 +0700

    meal pref apis and dummy foods

---
+++ backend/models/MealPrefModel.js
@@ -0,0 +1,54 @@
+// import connection
+import db from "../config/database.js";
+
+export const getMealPrefInfo = (data, result) => {
+    if (data.user_id) {
+        db.query("SELECT * FROM mealpref WHERE user_id = ?", [data.user_id], (err, results) => {
+            if (err) {
+                console.log(err);
+                result({ "status": 0, "message": "Can not get user's meal pref", "data": [] });
+            } else {
+                if (results[0]) {
+                    result({ "status": 1, "message": "Successfully get user's meal pref", "data": [results[0]] });
+                } else {
+                    result({ "status": 0, "message": "Can not get user's meal pref", "data": [] });
+                }
+            }
+        });
+    } else {
+        console.log(err);
+        result({ "status": 0, "message": "Can not get user's meal pref", "data": [] });
+    }
+}
+
+export const createMealPrefInfo = (data, result) => {
+    // pref_goal data:
+    // loss - 0
+    // maintain - 1
+    // gain - 2
+
+    // pref_date_range: 
+    // daily - 1
+    // weekly - 7
+    // monthly - 30
+
+    if (data.user_id) {
+        db.query("INSERT INTO user SET user_id = ?, pref_calo = ?, pref_time = ?, pref_goal = ?, pref_date_range = ?", [data.user_id, data.pref_calo, data.pref_time, pref_goal, pref_date_range], (err, results) => {
+            if (err) {
+                console.log(err);
+                result({ "status": 0, "message": "Can not get user's meal pref", "data": [] });
+            } else {
+                if (results[0]) {
+                    result({ "status": 1, "message": "Successfully get user's meal pref", "data": [results[0]] });
+                } else {
+                    result({ "status": 0, "message": "Can not get user's meal pref", "data": [] });
+                }
+            }
+        });
+    } else {
+        console.log(err);
+        result({ "status": 0, "message": "Can not create user's meal pref", "data": [] });
+    }
+}
+
+export const updateMealPrefInfo = (data, result) => { }
