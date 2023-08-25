5d3f1ff89d57c7292d0d1ef286a42480e5c60b64
103430040 <chiphamleyen@gmail.com>
2023-08-25 21:51:19 +0700

    get data for RecipeDetail with default foodId = 1

--- android/app/src/main/java/com/example/nutrichief/view/RecipeDetail.kt
+++ android/app/src/main/java/com/example/nutrichief/view/RecipeDetail.kt
@@ -2,17 +2,28 @@ package com.example.nutrichief.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nutrichief.R
 import com.example.nutrichief.adapter.IngredientAdapter
 import com.example.nutrichief.datamodels.Ingredient
 import com.example.nutrichief.datamodels.RecipeIngredient
+import com.example.nutrichief.datamodels.User
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONObject
 import java.io.IOException
+import java.time.LocalDate
 
 class RecipeDetail : AppCompatActivity() {
     private lateinit var ingredientRecyclerView: RecyclerView
@@ -26,56 +37,78 @@ class RecipeDetail : AppCompatActivity() {
         setContentView(R.layout.activity_recipe_detail)
 
         ingredientRecyclerView = findViewById(R.id.ingredients_recycler_view)
-//        ingredientRecyclerView.layoutManager = LinearLayoutManager(this)
-//        val dummy = mutableListOf<RecipeIngredient>(
-//            RecipeIngredient(
-//            1,
-//            Ingredient(1, "noodle", 100f, 100, 10f, 10f, 10f, "") ,
-//            100f),
-//            RecipeIngredient(
-//                1,
-//                Ingredient(2, "egg", 101f, 100, 10f, 10f, 10f, "") ,
-//                100f),
-//            RecipeIngredient(
-//                1,
-//                Ingredient(3, "pork", 102f, 100, 10f, 10f, 10f, "") ,
-//                100f),
-//        )
-//        ingredientRecyclerView.adapter = IngredientAdapter(dummy)
 
         val foodId = 1 // Replace with the desired food_id
-        val requestBody = FormBody.Builder()
-            .add("food_id", foodId.toString())
-            .build()
 
-        val request = Request.Builder()
-            .url("http://10.0.2.2:8001/apis/recipe/ingre") // Replace with your actual API URL
-            .post(requestBody)
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onResponse(call: Call, response: Response) {
-                val responseBody = response.body?.string()
-                val ingredientList: List<RecipeIngredient> = parseJson(responseBody)
-
+        getRecipeData(foodId) { recipeIngredients ->
+            recipeIngredients?.let {
                 runOnUiThread {
-                    adapter = IngredientAdapter(ingredientList as MutableList<RecipeIngredient>)
-                    ingredientRecyclerView.adapter = adapter
+                    adapter = IngredientAdapter(it as MutableList<RecipeIngredient>)
                     ingredientRecyclerView.layoutManager = LinearLayoutManager(this@RecipeDetail)
+                    ingredientRecyclerView.adapter = adapter
                 }
+            } ?: run {
+                // Handle the case when recipeIngredients is null (error occurred)
+                Toast.makeText(this, "Failed to retrieve recipe ingredients", Toast.LENGTH_SHORT).show()
+                Log.e("RecipeDetail", "Failed to retrieve recipe ingredients")
             }
-
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
-                // Handle failure
-            }
-        })
+        }
     }
 
-    private fun parseJson(json: String?): List<RecipeIngredient> {
-        val gson = Gson()
-        return gson.fromJson(json, Array<RecipeIngredient>::class.java).toList()
+    private fun getRecipeData(foodId: Int, callback: (List<RecipeIngredient>?) -> Unit) {
+        GlobalScope.launch(Dispatchers.Main) {
+            try {
+                val requestBody = JSONObject()
+                requestBody.put("food_id", foodId)
+
+                val request = Request.Builder()
+                    .url("http://10.0.2.2:8001/apis/recipe/ingre")
+                    .post(RequestBody.create("application/json".toMediaTypeOrNull(), requestBody.toString()))
+                    .build()
+
+                val response = withContext(Dispatchers.IO) { client.newCall(request).execute() }
+
+                if (!response.isSuccessful) {
+                    throw IOException("Failed to retrieve recipe ingredients")
+                }
+
+                val responseBody = response.body?.string()
+                val resultJson = JSONObject(responseBody ?: "")
+                val status = resultJson.optInt("status", 0)
+
+                if (status == 1) {
+                    val data = resultJson.optJSONArray("data")
+
+                    val recipeIngredients = mutableListOf<RecipeIngredient>()
+                    for (i in 0 until data.length()) {
+                        val ingredientJson = data.optJSONObject(i)
+                        val ingredient = Ingredient(
+                            ingredientJson.getInt("ingre_id"),
+                            ingredientJson.getString("ingre_name"),
+                            ingredientJson.getDouble("ingre_price").toFloat(),
+                            ingredientJson.getInt("ingre_calo"),
+                            ingredientJson.getDouble("ingre_fat").toFloat(),
+                            ingredientJson.getDouble("ingre_protein").toFloat(),
+                            ingredientJson.getDouble("ingre_carb").toFloat(),
+                            ingredientJson.getString("ingre_img")
+                        )
+                        val recipeQty = ingredientJson.getDouble("recipe_qty").toFloat()
+
+                        val recipeIngredient = RecipeIngredient(foodId, ingredient, recipeQty)
+                        recipeIngredients.add(recipeIngredient)
+                    }
+                    callback(recipeIngredients)
+                } else {
+                    callback(null)
+                }
+            } catch (e: Exception) {
+                // Handle the error here
+                callback(null)
+                Log.e("RecipeDetail", "Failed to retrieve recipe ingredients: ${e.message}")
+            }
+        }
     }
+
 
     fun goBack(view: View) {}
 }
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,14 +1,19 @@
-// import express 
+// import express
 import express from "express";
 
-import {
-    createOtp, verifyOtp
-} from "../controllers/otp.js";
+import { createOtp, verifyOtp } from "../controllers/otp.js";
+
+import { getUser, updateUser } from "../controllers/user.js";
 
 import {
-    getUser,
-    updateUser
-} from "../controllers/user.js";
+    getMealPref,
+    createMealPref,
+    updateMealPref,
+} from "../controllers/mealpref.js";
+
+import { getAllFoods } from "../controllers/food.js";
+
+import { getIngres, getIngresFromRecipe } from "../controllers/ingre.js";
 
 // init express router
 const router = express.Router();
@@ -20,13 +25,33 @@ router.post("/apis/otp/create", createOtp);
 // verify otp user
 router.post("/apis/otp/verify", verifyOtp);
 
-
 ////////////////////////// USER ////////////////////////////////
 // get user by id
 router.post("/apis/user/get", getUser);
 
 // update user
 router.post("/apis/user/update", updateUser);
+
+//////////////////////// MEAL PREF //////////////////////////////
+// get meal pref by user id
+router.post("/apis/mealpref/get", getMealPref);
+
+// create meal pref
+router.post("/apis/mealpref/create", createMealPref);
+
+// update meal pref
+router.post("/apis/mealpref/update", updateMealPref);
+
+////////////////////////// FOOD ////////////////////////////////
+// get foods
+router.post("/apis/food", getAllFoods);
+
+////////////////////////// INGRE ////////////////////////////////
+// get ingres
+router.post("/apis/ingre", getIngres);
+
+// get ingres
+router.post("/apis/recipe/ingre", getIngresFromRecipe);
 
 // export default router
 export default router;
