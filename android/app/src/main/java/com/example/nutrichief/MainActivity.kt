b9fb274b6463f8b6e723b73a799bcff32dc789fa
103430040 <chiphamleyen@gmail.com>
2023-08-27 01:38:25 +0700

    Ingredients Search

--- android/app/src/main/java/com/example/nutrichief/MainActivity.kt
+++ android/app/src/main/java/com/example/nutrichief/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.nutrichief
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.test)
+        text.setOnClickListener {
+            val fragmentContainer: FragmentContainerView = findViewById(R.id.fragment_container)
+            fragmentContainer.visibility = View.VISIBLE
+
+//            val intent = Intent(this, SearchActivity::class.java)
+//            startActivity(intent)
+        }
     }
 }
--- android/app/src/main/java/com/example/nutrichief/view/SearchFragment.kt
+++ android/app/src/main/java/com/example/nutrichief/view/SearchFragment.kt
@@ -1,68 +1,155 @@
 package com.example.nutrichief.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.nutrichief.R
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.nutrichief.adapter.IngredientSearchAdapter
+import com.example.nutrichief.datamodels.Ingredient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONObject
+import java.io.IOException
 
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+private lateinit var searchRecyclerView: RecyclerView
+//    private lateinit var dishAdapter: DishAdapter
+    private lateinit var ingredientAdapter: IngredientSearchAdapter
+//    private lateinit var allDishes: List<Dish>
+    private lateinit var allIngredients: List<Ingredient>
+    private var isSearchingIngredients = false
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_search, container, false)
 
         val dishBtn = view.findViewById<Button>(R.id.dishButton)
-        val ingreBtn = view.findViewById<Button>(R.id.ingredientButton)
+        val ingredientBtn = view.findViewById<Button>(R.id.ingredientButton)
+        val searchBox = view.findViewById<SearchView>(R.id.search_menu_items)
 
-        dishBtn.setOnClickListener{
+        searchRecyclerView = view.findViewById(R.id.ingre_recycler_view)
+        searchRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // Initialize adapters
+//        dishAdapter = DishAdapter(requireContext(), mutableListOf())
+        ingredientAdapter = IngredientSearchAdapter(mutableListOf())
+
+        // Set the initial adapter to dishAdapter
+//        searchRecyclerView.adapter = dishAdapter
+
+        dishBtn.setOnClickListener {
+            isSearchingIngredients = false
+//            searchRecyclerView.adapter = dishAdapter
+            // Replace this with your actual data retrieval method for dishes
+//            allDishes = getAllDishes()
+//            dishAdapter.filterList(allDishes)
+        }
+
+        ingredientBtn.setOnClickListener {
+            isSearchingIngredients = true
+
+            getAllIngredients { ingredients ->
+                if (ingredients != null) {
+                    allIngredients = ingredients
+                    ingredientAdapter.filterList(allIngredients as MutableList<Ingredient>)
+                    ingredientAdapter = IngredientSearchAdapter(allIngredients as MutableList<Ingredient>)
+                    searchRecyclerView.adapter = ingredientAdapter
+                } else {
+                    Log.e("Ingredients Search", "Failed to retrieve recipe ingredients")
+                }
+            }
+//            ingredientAdapter.filter.filter(searchBox.query) // Apply search filter
+
 
         }
 
-        ingreBtn.setOnClickListener{
-            ingreBtn.isEnabled = true
-            dishBtn.isEnabled = false
-        }
+        searchBox.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
 
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Log.d("SearchFragment", "New text: $newText")
+                if (isSearchingIngredients) {
+                    ingredientAdapter.filter.filter(newText)
+                    return true
+                }
+                return false
+            }
+        })
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun getAllIngredients(callback: (List<Ingredient>?) -> Unit){
+        GlobalScope.launch(Dispatchers.Main) {
+            try {
+                val requestBody = JSONObject()
+
+                val request = Request.Builder()
+                    .url("http://10.0.2.2:8001/apis/ingre")
+                    .post(
+                        RequestBody.create(
+                            "application/json".toMediaTypeOrNull(),
+                            requestBody.toString()
+                        )
+                    )
+                    .build()
+
+                val response = withContext(Dispatchers.IO) { client.newCall(request).execute() }
+
+                if (!response.isSuccessful) {
+                    throw IOException("Failed to retrieve recipe ingredients")
                 }
+
+                val responseBody = response.body?.string()
+                val resultJson = JSONObject(responseBody ?: "")
+                val status = resultJson.optInt("status", 0)
+
+                if (status == 1) {
+                    val data = resultJson.optJSONArray("data")
+
+                    val ingredients = mutableListOf<Ingredient>()
+                    for (i in 0 until data.length()) {
+                        val jsonIngredient: JSONObject = data.getJSONObject(i)
+                        val ingredient = Ingredient(
+                            jsonIngredient.getInt("ingre_id"),
+                            jsonIngredient.getString("ingre_name"),
+                            jsonIngredient.getDouble("ingre_price").toFloat(),
+                            jsonIngredient.getInt("ingre_calo"),
+                            jsonIngredient.getDouble("ingre_fat").toFloat(),
+                            jsonIngredient.getDouble("ingre_protein").toFloat(),
+                            jsonIngredient.getDouble("ingre_carb").toFloat(),
+                            jsonIngredient.getString("ingre_img")
+                        )
+                        ingredients.add(ingredient)
+                    }
+                    callback(ingredients)
+                } else {
+                    callback(null)
+                }
+            }catch (e: Exception) {
+                // Handle the error here
+                Log.e("RecipeDetail", "Failed to retrieve recipe ingredients: ${e.message}")
             }
+        }
     }
 }
