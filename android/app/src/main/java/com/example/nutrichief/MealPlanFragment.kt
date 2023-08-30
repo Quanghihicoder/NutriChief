a8b566c16a1e7c2992872f235934142d7b9e2dca
Luu Dao Dung Tri <luudaodungtri@gmail.com>
2023-08-30 17:50:30 +0700

    add dropdown menu at meal plan fragment

--- android/app/src/main/java/com/example/nutrichief/MealPlanFragment.kt
+++ android/app/src/main/java/com/example/nutrichief/MealPlanFragment.kt
@@ -2,25 +2,23 @@ package com.example.nutrichief
 
 import android.os.Build
 import android.os.Bundle
-import android.os.Parcelable
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.transition.Visibility
 import com.example.nutrichief.model.Food
 import com.example.nutrichief.model.Meal
-import java.util.ArrayList
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_MEAL_LIST = "meal_list"
+
 /**
  * A simple [Fragment] subclass.
  * Use the [MealPlanFragment.newInstance] factory method to
@@ -30,6 +28,7 @@ class MealPlanFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var mealList = mutableListOf<Meal>()
     private lateinit var recyclerViewMeal: RecyclerView
+    private var date = LocalDate.now()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -39,7 +38,6 @@ class MealPlanFragment : Fragment() {
                 it.getParcelableArrayList<Meal>(ARG_MEAL_LIST) as MutableList<Meal>
             }
         }
-
     }
 
     override fun onCreateView(
@@ -50,7 +48,51 @@ class MealPlanFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_meal_plan, container, false)
         val textViewGeneratePlan = view.findViewById<TextView>(R.id.text_view_generate_plan)
         val layoutMealPlanOptions = view.findViewById<LinearLayout>(R.id.layout_meal_plan_options)
-        val layoutSuggestedMealPlan = view.findViewById<FrameLayout>(R.id.layout_suggested_meal_plan)
+        val layoutSuggestedMealPlan =
+            view.findViewById<FrameLayout>(R.id.layout_suggested_meal_plan)
+        val textViewDate = view.findViewById<TextView>(R.id.text_view_date)
+        val imageViewMealPlanActions = view.findViewById<ImageView>(R.id.image_view_meal_plan_actions)
+        textViewDate.text = DateTimeFormatter.ofPattern("dd MMM yyyy").format(date)
+
+        val imageViewBackward = view.findViewById<ImageView>(R.id.image_view_backward)
+        val imageViewForward = view.findViewById<ImageView>(R.id.image_view_forward)
+
+        imageViewBackward.setOnClickListener {
+            date = date.minusDays(1)
+            textViewDate.text = DateTimeFormatter.ofPattern("dd MMM yyyy").format(date)
+        }
+
+        imageViewForward.setOnClickListener {
+            date = date.plusDays(1)
+            textViewDate.text = DateTimeFormatter.ofPattern("dd MMM yyyy").format(date)
+        }
+
+//        ArrayAdapter.createFromResource(
+//            this.context!!, R.array.spinner_meal_plan_actions,
+//            android.R.layout.simple_spinner_item
+//        ).also { adapter ->
+//            // Specify the layout to use when the list of choices appears
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//            // Apply the adapter to the spinner
+//            spinnerMealPlanActions.adapter = adapter }
+        imageViewMealPlanActions.setOnClickListener {
+            val popUpMenu = PopupMenu(requireContext(), it)
+            val menuInflater = popUpMenu.menuInflater
+            menuInflater.inflate(R.menu.meal_plan_action_menu, popUpMenu.menu)
+
+//            popupMenu.setOnMenuItemClickListener { item ->
+//                when (item.getItemId()) {
+//                    R.id.menu_item_option1 ->                 // Handle option 1 click
+//                        return@setOnMenuItemClickListener true
+//                    R.id.menu_item_option2 ->                 // Handle option 2 click
+//                        return@setOnMenuItemClickListener true
+//                    else -> return@setOnMenuItemClickListener false
+//                }
+//            }
+
+            popUpMenu.show()
+        }
+
         textViewGeneratePlan.setOnClickListener {
             val breakfastFoods = mutableListOf(
                 Food(1, "Banh Mi", "good", 0, 10),
@@ -88,8 +130,10 @@ class MealPlanFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         if (mealList.isNotEmpty()) {
-            val layoutMealPlanOptions = this.view?.findViewById<LinearLayout>(R.id.layout_meal_plan_options)
-            val layoutSuggestedMealPlan = this.view?.findViewById<FrameLayout>(R.id.layout_suggested_meal_plan)
+            val layoutMealPlanOptions =
+                this.view?.findViewById<LinearLayout>(R.id.layout_meal_plan_options)
+            val layoutSuggestedMealPlan =
+                this.view?.findViewById<FrameLayout>(R.id.layout_suggested_meal_plan)
             layoutMealPlanOptions?.visibility = View.GONE
             layoutSuggestedMealPlan?.visibility = View.VISIBLE
         }
