e4fffa3c8800a3ce6af3d46b1d90c324cab8cb98
103565751 <tranducanhsbvn@gmail.com>
2023-08-31 08:11:43 +0700

    sql

--- android/app/src/main/java/com/example/nutrichief/view/InstructionsActivity.kt
+++ android/app/src/main/java/com/example/nutrichief/view/InstructionsActivity.kt
@@ -10,7 +10,6 @@ import android.widget.*
 import com.example.nutrichief.R
 import com.example.nutrichief.datamodels.Ingredient
 import com.example.nutrichief.datamodels.RecipeIngredient
-import com.squareup.picasso.Picasso
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -79,6 +78,9 @@ class InstructionsActivity : AppCompatActivity() {
                     totalPages = cookingSteps.size
 
                     videoPath = cookingSteps[currentPage - 1].media_url
+                    if (videoPath == "") {
+                        videoPath = "https://www.shutterstock.com/shutterstock/videos/1009023404/preview/stock-footage-rapidly-chopping-onion-close-up-slow-mothion-red-onions-close-up-female-hands-cut-onions-in.webm"
+                    }
                     mediaController.setAnchorView(videoView)
                     videoView.setMediaController(mediaController)
                     videoView.setVideoURI(Uri.parse(videoPath))
