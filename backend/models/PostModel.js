af493e3fd00891bb55e9a33518d1131c18b0929c
Quanghihicoder <hoangquang2508@gmail.com>
2023-08-31 09:35:02 +0700

    advanced community apis

--- backend/models/PostModel.js
+++ backend/models/PostModel.js
@@ -2,7 +2,7 @@
 import db from "../config/database.js";
 
 export const getAllPosts = (result) => {
-    db.query("SELECT * FROM post ORDER BY post_id DESC", [], (err, results) => {
+    db.query("SELECT a.post_id, a.user_id, a.post_title, a.post_detail, a.post_dislike, b.post_like FROM ((SELECT post.post_id, post.user_id, post.post_title, post.post_detail, IFNULL(d.d,0) AS post_dislike FROM post LEFT JOIN (SELECT post_id ,COUNT(user_id) AS d FROM react WHERE react_type = 0 GROUP BY post_id) as d ON post.post_id = d.post_id)) AS a LEFT JOIN ((SELECT post.post_id, IFNULL(l.l,0) AS post_like FROM post LEFT JOIN (SELECT post_id ,COUNT(user_id) AS l FROM react WHERE react_type = 1 GROUP BY post_id) as l ON post.post_id = l.post_id)) AS b ON a.post_id = b.post_id ORDER BY post_id DESC", [], (err, results) => {
         if (err) {
             console.log(err);
             result({ status: 0, message: "Can not get posts", data: [] });
@@ -46,5 +46,3 @@ export const createUserPost = (data, result) => {
         result({ status: 0, message: "Can not create post", data: [] });
     }
 };
-
-// SELECT COUNT(*) FROM react WHERE post_id = 1 GROUP BY react_type;
--- backend/models/ReactModel.js
+++ backend/models/ReactModel.js
@@ -1,6 +1,36 @@
 // import connection
 import db from "../config/database.js";
 
+export const getUserReactByPost = (data, result) => {
+    if (data.user_id && data.post_id) {
+        db.query(
+            "SELECT * FROM react WHERE user_id = ? AND post_id = ?",
+            [data.user_id, data.post_id],
+            (err, results) => {
+                if (results[0]) {
+                    result({
+                        status: 1,
+                        message: "Successfully get react",
+                        data: [{ react_type: results[0].react_type }],
+                    });
+                } else {
+                    result({
+                        status: 1,
+                        message: "Did not react",
+                        data: [{ react_type: null }],
+                    });
+                }
+            }
+        );
+    } else {
+        result({
+            status: 0,
+            message: "Can not get react",
+            data: [],
+        });
+    }
+}
+
 export const updateReactByUser = (data, result) => {
     db.query(
         "DELETE FROM react WHERE user_id = ? AND post_id = ?",
